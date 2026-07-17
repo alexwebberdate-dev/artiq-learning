e8a17da53a59813bbbf6d5b0b9376f23c6da74d1
Alex Webber-Date <alexwebberdate@gmail.com>
2026-07-17 18:56:54 +0100

    Chanegd the number of points in the fake measurement to see the applet updating

--- experiments/fake_measurement.py
+++ experiments/fake_measurement.py
@@ -7,12 +7,12 @@ class Fake_Measurement(EnvExperiment):
         self.setattr_argument("npoints", NumberValue(default=50, precision=0, step=1))
         self.setattr_argument("tau", NumberValue(default=10))
 
-        def run(self):
-            n = int(self.npoints)
-            self.set_dataset("signal", np.full(n, np.nan),
-                             broadcast = true)
+    def run(self):
+        n = int(self.npoints)
+        self.set_dataset("signal", np.full(n, np.nan),
+                             broadcast = True)
             
-            for i in range(n):
+        for i in range(n):
                 value = np.exp(-i /self.tau) + np.random.normal(0, 0.02)
                 self.mutate_dataset("signal", i, value)
                 time.sleep(0.1)
