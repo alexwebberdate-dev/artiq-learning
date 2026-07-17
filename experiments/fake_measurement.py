5661cb2cc8245e50f737a7082823aca915b354d0
Alex Webber-Date <alexwebberdate@gmail.com>
2026-07-17 18:34:19 +0100

    Changed greeting argument in hello, fake measurement setup

---
+++ experiments/fake_measurement.py
@@ -0,0 +1,18 @@
+from artiq.experiment import *
+import numpy as np
+import time
+
+class Fake_Measurement(EnvExperiment):
+    def build(self):
+        self.setattr_argument("npoints", NumberValue(default=50, precision=0, step=1))
+        self.setattr_argument("tau", NumberValue(default=10))
+
+        def run(self):
+            n = int(self.npoints)
+            self.set_dataset("signal", np.full(n, np.nan),
+                             broadcast = true)
+            
+            for i in range(n):
+                value = np.exp(-i /self.tau) + np.random.normal(0, 0.02)
+                self.mutate_dataset("signal", i, value)
+                time.sleep(0.1)
--- experiments/hello.py
+++ experiments/hello.py
@@ -3,7 +3,8 @@ from artiq.experiment import *
 class Hello(EnvExperiment):
     def build(self):
         self.setattr_argument("count", NumberValue(default=10, precision=0, step=1))
+        self.setattr_argument("greeting", StringValue(default= "Hello ARTIQ"))
 
     def run(self):
         for i in range(int(self.count)):
-            print("Hello ARTIQ", i)
+            print(self.greeting, i)
